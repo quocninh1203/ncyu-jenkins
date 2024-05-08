e6648d4bb77dccc852b4f242ae5b0f4b1c2d758f
origin <quocninh9.32017@gmail.com>
2024-05-08 10:00:11 +0700

    Test Code for Jenkins

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        GradeConverter converter = new GradeConverter();
+
+        for(int i=0; i<105; i++){
+            System.out.println(i+";"+converter.convert(i));
+        }
+    }
+}
